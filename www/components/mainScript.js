32e43e7b59ccd698be5b9f79e1b9887a8937038f
RonaldPombo <ronaldsljr16@gmail.com>
2023-03-14 00:07:06 +0900

    feito coisa pra krl

--- www/components/mainScript.js
+++ www/components/mainScript.js
@@ -1,2 +1,8 @@
-// This is a JavaScript file
-
+function Imc(){
+    var nome = documente.getElementById(nome)
+    var altura = parseFloat(document.getElementById('altura').value)
+    var peso = parseFloat(document.getElementById('peso').value)
+    document.getElementById('imc').style.marginLeft = "41vw"
+    var resultado = (peso / (altura * altura)).toFixed(2)
+    document.getElementById("imc").textContent = "Olá " + nome + "seu imc é" + resultado
+}
